1dafef23498daedfec16b1f1c057e70c480ee0a7
TDCIAN <tdcian71@gmail.com>
2022-05-26 23:26:31 +0900

    make project

---
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by JeongminKim on 2022/05/26.
+//
+
+import UIKit
+
+final class NewsListTableViewController: UITableViewController {
+    
+}
